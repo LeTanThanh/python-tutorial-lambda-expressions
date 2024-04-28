2edf882bce726fc6f4d7aafb65d38c293f0bfc01
TanThanhLe <tanthanhcr95@gmail.com>
2024-04-28 13:14:53 +0700

    What are Python lambda expressions

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
 if __name__ == "__main__":
-  print("Python Lambda Expressions")
+  # What are Python lambda expressions
+
+  """
+  Python lambda expressions allow you to define anonymous functions.
+
+  Anonymous functions are functions without names.
+  The anonymous functions are useful when you neef to use them once.
+
+  A lambda expression typically contains one or more arguments, but it can have only one expression.
+
+  The following shows the lambda expression syntax:
+
+  lambda parameters: expression
+
+  It's equivalent to the following function with the "anonymous" name:
+
+  def anonymous(parameters):
+    return expression
+  """
